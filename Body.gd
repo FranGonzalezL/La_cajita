9c440bfbb02c4565f97cc0b1a92d6aa45748c158
asjdio <asjdio>
2018-01-21 03:32:51 -0300

    Trying dynamic body for Tipito

--- Body.gd
+++ Body.gd
@@ -1,89 +1,48 @@
-extends KinematicBody2D
+extends RigidBody2D
 
 const GRAVITY = 60
-const WALK_SPEED = 140
+const WALK_SPEED = 500
 const JUMP_SPEED = 15
 const MAX_SPEED = 9
-const FRICTION = 100
 
 var velocity = Vector2(0, 0)
+var step = 0
 var state = "airborne"
 
 var up
 var left
 var right
 
-func update(delta):
+func _integrate_forces(state):
+	velocity = state.get_linear_velocity()
+	step = state.get_step()
+	
+	print(step)
+
 	# Update input flags
 	update_input()
+		
+	# Update velocity via input
+	if (left && !right):
+		velocity.x -= WALK_SPEED * step
+	elif (right && !left):
+		velocity.x += WALK_SPEED * step
 	
-	# Update velocity via gravity
-	velocity.y += GRAVITY * delta
+	state.set_linear_velocity(velocity)
 	
 	# Update velocity via input
-	if (state == "grounded"):
-		grounded_state(delta)
-	elif (state == "airborne"):
-		airborne_state(delta)
-	else:
-		print("State not found")
-		get_tree().quit()
+	#if (state == "grounded"):
+	#	grounded_state(delta)
+	#elif (state == "airborne"):
+	#	airborne_state(delta)
+	#else:
+	#	print("State not found")
+	#	get_tree().quit()
 	
-	# Move bitch
-	var motion = velocity
-	motion = move(motion)
-
-	# Floor detection
-	if (is_colliding()):
-		state = "grounded"
-	
-	# Collision resolution
-	if (is_colliding()):
-		# Push rigid object
-		var collider = get_collider()
-		if (collider extends RigidBody2D):
-			var impulse_direction = -get_collision_normal()
-			var impulse_norm = motion.dot(impulse_direction)
-			var relative_collision_pos = get_collision_pos() - collider.get_pos()
-			collider.apply_impulse(relative_collision_pos,
-			                       impulse_direction * impulse_norm)
-		# Rellocate
-		var n = get_collision_normal()
-		motion = n.slide(motion)
-		velocity = n.slide(velocity)
-		move(motion)
+	#if (is_colliding()):
+	#	state = "grounded"
 
 func update_input():
 	up = Input.is_action_pressed("ui_up")
 	left = Input.is_action_pressed("ui_left")
 	right = Input.is_action_pressed("ui_right")
-	
-func grounded_state(delta):
-	if (up):
-		velocity.y = -JUMP_SPEED
-		state = "airborne"
-	elif (left && !right):
-		velocity.x -= WALK_SPEED * delta
-	elif (right && !left):
-		velocity.x += WALK_SPEED * delta
-	else:
-		var is_negative = velocity.x < 0
-		var module = max(0, abs(velocity.x) - FRICTION * delta)
-		if is_negative:
-			velocity.x = -module
-		else:
-			velocity.x = module
-	clamp_xvelocity()
-
-func airborne_state(delta):
-	if (left && !right):
-		velocity.x -= WALK_SPEED * delta
-	elif (right && !left):
-		velocity.x += WALK_SPEED * delta
-	clamp_xvelocity()
-
-func clamp_xvelocity():
-	if velocity.x > MAX_SPEED:
-		velocity.x = MAX_SPEED
-	elif velocity.x < - MAX_SPEED:
-		velocity.x = -MAX_SPEED
--- Cajita.gd
+++ Cajita.gd
@@ -1,4 +1,13 @@
 extends RigidBody2D
 
+var applied_velocity = Vector2(0, 0)
+var timer = 0
+
 func _integrate_forces(state):
 	pass
+
+#func moveIt(delta):
+#	if (is_colliding()):
+#		var collider = get_collider()
+#		if (collider extends KinematicBody):
+#			self.add_force(Vector2(0,0), Vector2(30, 0))
